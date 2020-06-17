2a0c04211c03cf8adc68ce43f73f8d51606fc605
J Preethaa <57058479+geop-hub@users.noreply.github.com>
2020-06-17 19:25:05 +0530

    Update image_classification.py

--- image_classification.py
+++ image_classification.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[9]:
-
-
 #importing dependcies
 import pandas as pd
 import numpy as np
@@ -12,66 +6,32 @@ from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 get_ipython().run_line_magic('matplotlib', 'inline')
 
-
-# In[2]:
-
-
+#reading the csv as a dataframe
 data = pd.read_csv('mnist_test.csv')
 
-
-# In[4]:
-
-
+#showing the first five rows of the dataframe
 data.head()
 
-
-# In[14]:
-
-
+#plotting one of the rows
 a=data.iloc[2,1:].values
 a=a.reshape(28,28).astype('uint8')
 plt.imshow(a)
 
-
-# In[6]:
-
-
+#splitting the data into features and labels
 data_x=data.iloc[:,1:]
 data_y=data.iloc[:,0]
 
-
-# In[7]:
-
-
+#splitting the data into train and test sets
 X_train,X_test,y_train,y_test=train_test_split(data_x,data_y,random_state=1,test_size=0.2)
 
-
-# In[20]:
-
-
+#initialising the classifier
 rfc=RandomForestClassifier(n_estimators=10)
 
-
-# In[21]:
-
-
+#training the model
 rfc.fit(X_train,y_train)
-
-
-# In[22]:
-
-
 pred=rfc.predict(X_test)
 
-
-# In[23]:
-
-
+#checking the accuracy
 rfc.score(X_test,y_test)
-
-
-# In[ ]:
-
-
 
 
